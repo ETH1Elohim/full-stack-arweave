60f0d0851d88d6fa12e029bb6a94a27c0395e604
ETH1Elohim <ezmax.defi@gmail.com>
2023-10-09 16:54:14 -0600

    SmartWeave contract deployment

---
+++ warp/createPost.js
@@ -0,0 +1,17 @@
+import { warp, configureWallet } from './configureWarpServer.js'
+import { transactionId } from '../transactionid.js'
+import { v4 as uuid } from 'uuid'
+
+async function createPost() {
+    let wallet = await configureWallet()
+    const contract = warp.contract(transactionId).connect(wallet)
+    await contract.writeInteractions({
+        function: "createPost",
+        post: {
+            title: "Hi from first post!",
+            content: "This is my first post!",
+            id: uuid()
+        }
+    })
+}
+createPost()
